e15b83a6380c2ad630b77bc759bfc5adec0d2ec6
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-09 12:47:01 +0530

    Add files via upload

---
+++ Day1.1.py
@@ -0,0 +1,10 @@
+# My First Day of Learning Python! (Date: 2025-02-09)
+# Excited to start my coding journey with Python! 🐍
+# Let's write some awesome code! 💻🔥
+
+
+
+# Escap sequance character
+print("Hey my name is:\nAnand prakash") #\n is a escape sequence character
+print('Hey my name is:\nAnand prakash"') #single quote
+print('''Hey my name is:\nAnand prakash''') #\n triple quote
