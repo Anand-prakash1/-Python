e4ed448ba1b17ebb40b136e571cbba8e2049b138
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-11 19:03:49 +0530

    Add files via upload

---
+++ Day2.5.py
@@ -0,0 +1,26 @@
+# Data type
+
+name = "Anand prakash"  #String data type
+age = 21 #integer data type
+Student = True #boolean data type
+marks = 79.8 #float data type
+
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(Student)
+print(type(Student))
+print(marks)
+print(type(marks))
+
+a = 18
+b = 18
+c = a + b 
+print(c)  #addition between two integers
+
+first_name = "Anand" #we cant use space between variables that why i use _ symbol
+last_name = "Prakash"
+
+full_name = first_name + " " + last_name #String concatination 
+print(full_name)
