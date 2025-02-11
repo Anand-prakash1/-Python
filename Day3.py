b6757415d122e1a153f5a41ac10aac2a506ea73d
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-11 19:07:00 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,10 @@
+a = 10 
+b = 5
+
+print(a // b) #find exact value after division
+print(a / b) #division
+print(a * b) #multiplication
+print(a + b) #addition
+print(a - b) #subtracton
+print(a ** b) #square of two number
+print(a % b) #remainder of two number
