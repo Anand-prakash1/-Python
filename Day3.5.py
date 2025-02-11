37b8942967651ca09c292782130753baf0f4093d
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-11 19:07:19 +0530

    Add files via upload

---
+++ Day3.5.py
@@ -0,0 +1,9 @@
+# Typecasting
+
+num1 = "12"  #integer value in the form of string
+updated_num1 = int(num1) #conver String into integer
+print(type(updated_num1))
+
+Str = 23
+updated_Str = str(Str) #convert integer value into string
+print(type(updated_Str))
