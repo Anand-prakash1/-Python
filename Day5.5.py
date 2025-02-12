13ba54129fcc5ef13ad5e7374bd6ae663ef6e5f6
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-12 08:44:59 +0530

    Add files via upload

---
+++ Day5.5.py
@@ -0,0 +1,6 @@
+Name = """paragraph is a group of sentences 
+that support a single idea. Paragraphs are a way 
+to organize written work and help readers understand the main points. 
+"""
+print(Name[0:20:2]) #Slicing 0 index to 19 and gapping one
+print(Name[-1:-5])
