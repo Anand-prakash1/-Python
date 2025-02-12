9fb2b79b738cb3bf9a708235f50e0e13da4f2428
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-12 08:44:13 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,7 @@
+# taking user input
+
+name = input("Enter your name here: ") #string input
+print("Your name is: ",name)
+
+num1 = int(input("Enter your number here: ")) #integer input
+print("Your number is: ",num1)
