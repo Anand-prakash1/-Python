ed046effea4ec4914e28619f31cf4d33306287c5
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-14 06:55:22 +0530

    Add files via upload

---
+++ question/even_odd.py
@@ -0,0 +1,8 @@
+# check even or odd using conditional statement
+
+num1 = int(input("Enter your number: "))
+
+if num1%2==0:
+    print("Your number is even")
+else:
+    print("Your number is odd")
