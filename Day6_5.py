a2af84d77aaa27c5daa86c0585b64449e66180ab
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-15 12:27:16 +0530

    Update Day6_5.py

--- Day6_5.py
+++ Day6_5.py
@@ -1,1 +1,12 @@
 
+a = "@@@@@@String@@@@@@@@"
+print(a.upper()) #Strings are immutable we cant 
+                 #change it but we create another
+print(a.lower())
+print(a.rstrip("@")) #cant change starting strip
+b = a.replace(a , "anand") #replace a variable to Anand
+print(b) 
+print(b.capitalize()) #capitalize first letter
+print(b.count("n")) #count the number of n in string
+print(a.find("a")) #there in no a letter n a variable 
+print(b.find("n")) #index of n
