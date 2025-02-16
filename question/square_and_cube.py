cfa459c8cfa257e46f081b708f1ee0d9d7f54196
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-16 17:29:58 +0530

    Update square_and_cube.py

--- question/square_and_cube.py
+++ question/square_and_cube.py
@@ -1,1 +1,9 @@
+# Square and cube of a number
 
+num1 = int(input("Enter your first number: "))
+
+square = num1 ** 2 #num1 * num1
+cube = num1 ** 3 #num1 * num1 * num1
+
+print("square of num1 is: ", square)
+print("cube of num1 is: ", cube)
