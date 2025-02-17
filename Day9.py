e3b4691a24e08ef9b40755ec3001fbb2ca5855ac
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-17 06:02:39 +0530

    Update Day9.py

--- Day9.py
+++ Day9.py
@@ -1,1 +1,10 @@
+# dictionary
+dict = {'key':'value',} #dictionary created
+# dict1 = dict.append({'apple':'sew', 'mango':'aam', 'banana':'kela'})
+print(type(dict))
 
+# we cant add element using append method 
+# Use Update method instead of append
+
+dict.update({'apple':'sew', 'mango':'aam', 'banana':'kela'}) #update values
+print(dict)
