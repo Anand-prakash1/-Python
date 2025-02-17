aaa38a300b05c81334e36eea77cffae8f7071471
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-17 06:05:17 +0530

    Create convertingfehrenheit.py

---
+++ question/convertingfehrenheit.py
@@ -0,0 +1,6 @@
+# celcius to fehrenheit
+
+cel = float(input("Enter your number finding fehrenheit: ")) #celcius input
+fehrenheit = (cel * 9/5) + 32  #Formula of fehrenheit
+
+print("celcius to fehrenheit:", fehrenheit)
