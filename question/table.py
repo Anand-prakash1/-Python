88d8da236efead2a28a04edbb2ec78221e5d1e68
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-18 11:24:37 +0530

    Update table.py

--- question/table.py
+++ question/table.py
@@ -1,1 +1,6 @@
+# Taking user input for the number
+num = int(input("Enter your number: "))
 
+# Generating the multiplication table
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")  # Using f-string for cleaner formatting
