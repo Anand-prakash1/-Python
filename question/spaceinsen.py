20c7999a8c74fdbb887d79f95a8603166657024a
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-18 11:23:21 +0530

    Update spaceinsen.py

--- question/spaceinsen.py
+++ question/spaceinsen.py
@@ -1,1 +1,10 @@
+# Taking user input for a sentence
+sentence = input("Enter Any sentence: ")
 
+# Checking for spaces in the sentence
+if "  " in sentence:  # Checking for double spaces first
+    print("Your sentence contains double spaces")
+elif " " in sentence:  # Checking for single spaces after
+    print("Your sentence contains a single space")
+else:  # If there are no spaces at all
+    print("Your sentence contains no spaces")
