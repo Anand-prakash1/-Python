691774a9f793a002c2e4260d8d860755e308fb39
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-20 11:57:09 +0530

    Update swaping.py

--- question/swaping.py
+++ question/swaping.py
@@ -1,1 +1,11 @@
 
+# swaping two variable 
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("After swaping: ", num1)
+print("After swaping: ", num2)
