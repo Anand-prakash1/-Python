fb87d5e62971a6e487436632ad2feb29320be640
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-22 23:08:50 +0530

    Update Goingschool.py

--- question/Goingschool.py
+++ question/Goingschool.py
@@ -1,1 +1,23 @@
+# Taking user input for weather condition
+weather = input("Enter weather info: ")  
 
+# Checking the weather condition using if-elif-else statements
+if weather == "sunny":
+    # If the weather is sunny, print this message
+    print("Today's weather is too good, go to school")  
+
+elif weather == "cloudy":
+    # If the weather is cloudy, print this message
+    print("Weather is ok, you can go to school")  
+
+elif weather == "rainy":
+    # If the weather is rainy, print this message (reminder to take an umbrella)
+    print("Today's a rainy day, don't forget an umbrella when going to school")  
+
+elif weather == "windy":
+    # If the weather is windy, print this caution message
+    print("Weather is too windy, be careful if you go to school")  
+
+else:
+    # If the user enters an invalid weather type, print an error message
+    print("Invalid weather. Please enter sunny, cloudy, rainy, or windy")  
