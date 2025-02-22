3b080af8a24703cce5b2fb5a7391b6f0b953efb4
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-22 23:09:55 +0530

    Update palendrom.py

--- question/palendrom.py
+++ question/palendrom.py
@@ -1,1 +1,15 @@
+# Taking user input for a number
+num = input("Enter your number: ")  
+
+# Reversing the number using slicing
+num2 = num[::-1]  
+
+# Printing the reversed number
+print("Reversed:", num2)  
+
+# Checking if the number is a palindrome
+if num == num2:  
+    print("Number is a palindrome")  # Corrected spelling
+else:  
+    print("Number isn't a palindrome")  # Corrected spelling
 
