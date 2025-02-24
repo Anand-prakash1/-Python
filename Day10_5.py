8e13a4f4f4d0922aaaf1a3f7dedaee0a5b9febe6
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-24 11:16:24 +0530

    Update Day10_5.py

--- Day10_5.py
+++ Day10_5.py
@@ -1,1 +1,11 @@
+# Initialize the count variable to 0
+count = 0  
+
+# Run the loop while count is less than 5
+while(count < 5):  
+    # Print "Hello world" in each iteration
+    print("Hello world")  
+    
+    # Increment count by 1 to avoid an infinite loop
+    count = count + 1  
 
