269198d9d43766b74af879aeff57cc3596e139dc
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-24 11:18:01 +0530

    Update Day11.py

--- Day11.py
+++ Day11.py
@@ -1,1 +1,9 @@
-
+# Loop through numbers from 0 to 14
+for i in range(15):  
+    # Print the multiplication result (incorrect calculation due to +1)
+    print("5 x", i, "=", 5 * i + 1)  
+    
+    # Stop the loop when i reaches 10
+    if i == 10:  
+        break  
+    print("Loop end")
