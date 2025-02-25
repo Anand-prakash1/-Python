3b8fcd6ad41ba8f6cd742d30ead15c7c0d2e1a47
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-02-25 09:48:10 +0530

    Update Day12_5.py

--- Day12_5.py
+++ Day12_5.py
@@ -1,1 +1,12 @@
+import statistics
 
+# Define the function with a parameter
+def CalculateMedian(data):
+    median = statistics.median(data)  # Compute median
+    print("Median:", median)  # Print result
+
+# Take input as a list of numbers
+data = list(map(int, input("Enter numbers separated by space: ").split()))
+
+# Call the function with the data
+CalculateMedian(data)
