b70a9e15a83f0c6a4608f7f43c53aebc7523559e
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-03-03 18:57:50 +0530

    Update Day14_5.py

--- Day14_5.py
+++ Day14_5.py
@@ -1,1 +1,13 @@
 
+''' Write a Python function that takes a list as input 
+and returns a new list with duplicates removed, maintaining the 
+original order.'''
+
+# Define a list with duplicate elements
+list1 = list(map(int, input("Enter numbers separated by space: ").split()))
+
+# Using dict.fromkeys() to remove duplicates while maintaining order
+uniq_list = list(dict.fromkeys(list1))
+
+# Print the unique list
+print(uniq_list)  # Output
