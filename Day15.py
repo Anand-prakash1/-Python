d3bef596538f319e9ebd33d0a781d227b4399b01
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-03-05 18:34:57 +0530

    Update Day15.py

--- Day15.py
+++ Day15.py
@@ -1,1 +1,16 @@
+# tuples......
 
+# Define the first tuple with some integer values
+tup1 = (1, 2, 34, 53, 53, 63)
+
+# Define the second tuple with another set of integer values
+tup2 = (10, 20, 30, 40, 50, 60)
+
+# Concatenate both tuples to create a new tuple
+tup3 = tup1 + tup2  
+
+# Convert the tuple into a list, sort the elements, and convert it back to a tuple
+tup3 = tuple(sorted(tup3))  
+
+# Print the sorted tuple
+print(tup3)
