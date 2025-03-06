f0e32964247c93e2386cf529050aaac671644549
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-03-06 21:38:02 +0530

    Update Day16.py

--- Day16.py
+++ Day16.py
@@ -1,1 +1,12 @@
+# Define a string with placeholders `{}` for inserting values dynamically
+letter = "Hey my name is {} and I am from {}"
 
+# Define values for the placeholders
+name = "Anand"
+country = "India"
+
+# Use the format() method to replace `{}` with actual values (name and country)
+formatted_letter = letter.format(name, country)
+
+# Print the formatted string
+print(formatted_letter)
