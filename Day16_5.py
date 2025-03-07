31c2ef1e222555cfd25a35deae351c8ff24e4ba3
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-03-07 14:19:59 +0530

    Update Day16_5.py

--- Day16_5.py
+++ Day16_5.py
@@ -1,1 +1,11 @@
+def factorial(n):
+    if n == 0 or n == 1:  # Base case: Factorial of 0 or 1 is always 1
+        return 1
+    else:
+        return n * factorial(n - 1)  # Recursive call reducing n by 1 each time
 
+# Take user input and convert it to an integer
+n = int(input("Enter your number: "))
+
+# Call the factorial function and print the result
+print(f"Factorial of {n} is: {factorial(n)}")
