c27ead630e3bfa1007446f3c45d00453a987b36d
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-03-12 23:00:42 +0530

    Update dictionary2.py

--- question/dictionary2.py
+++ question/dictionary2.py
@@ -1,1 +1,20 @@
+# Question no.3
+'''
+Write a program to loop through a dictionary and 
+print all keys and values.
+'''
 
+fruits = {"Apple": 100, "Banana": 50, "Mango": 80}
+for key, value in fruits.items():
+    print(key, value)
+    
+# Question no.4
+'''
+Write a program that checks if a given key exists in a dictionary.
+'''
+
+fruits = {"Apple": 100, "Banana": 50, "Mango": 80}
+if "Apple" in fruits:
+    print("Yes")
+else:
+    print("No")
