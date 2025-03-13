d21e68f565cc59ec86e4753550d54323d2f97445
Anand prakash <155887470+Anand-prakash1@users.noreply.github.com>
2025-03-13 23:32:11 +0530

    Update even_odd_checker.py

--- question/even_odd_checker.py
+++ question/even_odd_checker.py
@@ -1,1 +1,13 @@
+'''Print Even and Odd Numbers
 
+Write a program to iterate through numbers from 1 to 10.
+Print "Even" if the number is even, else print "Odd".'''
+
+# Loop through numbers from 0 to 9
+for i in range(10):
+    
+    # Check if the number is even
+    if i % 2 == 0:
+        print(f"{i}: Even number")  # Print if even
+    else:
+        print(f"{i}: Odd number")   # Print if odd
